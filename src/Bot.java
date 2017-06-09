ec1caa783b2e5ee07f54e589cf47462e431ea113
grufelous <grufelous@gmail.com>
2017-06-09 19:51:01 +0530

    Added constructors

--- src/Bot.java
+++ src/Bot.java
@@ -1,2 +1,16 @@
+import static java.lang.System.out;
+
 public class Bot {
+    String level;
+    String[] levels = {"easy", "medium", "difficult"};
+    public Bot(String level) {
+        out.print(level);
+        for(int i = 0; i < 3; i++) {
+            if(level.equalsIgnoreCase(levels[i])) {
+                this.level = levels[i];
+                break;
+            }
+        }
+        out.printf("Level: %s\n", this.level);
+    }
 }
--- src/OEGame.java
+++ src/OEGame.java
@@ -3,5 +3,7 @@ import static java.lang.System.*;
 public class OEGame {
     public static void main(String[] args) {
         out.printf("Welcome to OEGame");
+        Bot b = new Bot("hard");
+        Bot c = new Bot("qwerty");
     }
 }
